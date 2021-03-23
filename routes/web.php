cfc4542e4d264735d0e2b0fc8ee1ab63159c581a
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-03-23 19:44:04 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController\LoginController;
 
-Route::get('refresh', function () {
+Route::get('migrate-refresh', function () {
+    \Artisan::call('migrate:refresh');
+    dd("Done");
+});
 
+Route::get('migrate', function () {
     \Artisan::call('migrate:refresh');
     dd("Done");
 });
 
 
+Route::get('clear', function () {
+
+    \Artisan::call('cache:clear');
+    \Artisan::call('view:clear');
+    \Artisan::call('config:cache');
+    // \Artisan::call('cache:clear');
+    dd("Done");
+});
 
